192015c6726b1d0869418d06de1f380bfe80d4ec
yoshidan <naohiro.y@gmail.com>
2024-12-26 11:59:49 +0900

    call preimage maker
    Signed-off-by: yoshidan <naohiro.y@gmail.com>

--- module/l2.go
+++ module/l2.go
@@ -1,7 +1,10 @@
 package module
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
+	"fmt"
 	"github.com/cockroachdb/errors"
 	"github.com/ethereum-optimism/optimism/op-service/client"
 	"github.com/ethereum-optimism/optimism/op-service/dial"
@@ -9,10 +12,13 @@ import (
 	"github.com/ethereum-optimism/optimism/op-service/sources"
 	log2 "github.com/ethereum/go-ethereum/log"
 	"github.com/hyperledger-labs/yui-relayer/log"
+	"io"
+	"net/http"
 )
 
 type L2Client struct {
 	rollupClient dial.RollupClientInterface
+	config       *ProverConfig
 }
 
 func NewL2Client(ctx context.Context, config *ProverConfig) (*L2Client, error) {
@@ -23,9 +29,12 @@ func NewL2Client(ctx context.Context, config *ProverConfig) (*L2Client, error) {
 	}
 	return &L2Client{
 		rollupClient: sources.NewRollupClient(rpc),
+		config:       config,
 	}, nil
 }
 
+// LatestDerivation retrieves the latest derivation information from the rollup client.
+// It fetches the sync status, claimed output, and agreed output for the latest blocks.
 func (c *L2Client) LatestDerivation(ctx context.Context) (*eth.L1BlockRef, *Derivation, error) {
 	syncStatus, err := c.rollupClient.SyncStatus(ctx)
 	if err != nil {
@@ -53,4 +62,53 @@ func (c *L2Client) LatestDerivation(ctx context.Context) (*eth.L1BlockRef, *Deri
 		L2BlockNumber:      claimedNumber,
 	}, nil
 
+}
+
+// SetupDerivations sets up a list of derivations between the trusted height and the latest agreed number.
+// It iterates from the trusted height to the latest agreed number, fetching the agreed and claimed outputs
+// for each block and appending them to the derivations list.
+func (c *L2Client) SetupDerivations(ctx context.Context, trustedHeight uint64, latestAgreedNumber uint64) ([]*Derivation, error) {
+	derivations := make([]*Derivation, 0)
+	for i := trustedHeight; i < latestAgreedNumber; i++ {
+		agreedNumber := trustedHeight
+		agreedOutput, err := c.rollupClient.OutputAtBlock(ctx, agreedNumber)
+		if err != nil {
+			return nil, errors.WithStack(err)
+		}
+		claimedNumber := agreedNumber + 1
+		claimedOutput, err := c.rollupClient.OutputAtBlock(ctx, claimedNumber)
+		if err != nil {
+			return nil, errors.WithStack(err)
+		}
+		derivations = append(derivations, &Derivation{
+			AgreedL2HeadHash:   agreedOutput.BlockRef.Hash.Bytes(),
+			AgreedL2OutputRoot: agreedOutput.OutputRoot[:],
+			L2HeadHash:         claimedOutput.BlockRef.Hash.Bytes(),
+			L2OutputRoot:       claimedOutput.OutputRoot[:],
+			L2BlockNumber:      claimedNumber,
+		})
+	}
+	return derivations, nil
+}
+
+// CreatePreimages sends a list of derivations to the preimage maker service and returns the preimage data.
+// It marshals the derivations into JSON, sends a POST request to the preimage maker endpoint, and reads the response.
+func (c *L2Client) CreatePreimages(ctx context.Context, derivations []*Derivation) ([]byte, error) {
+	httpClient := http.Client{
+		Timeout: c.config.PreimageMakerTimeout
+	}
+	body, err := json.Marshal(derivations)
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+	buffer := bytes.NewBuffer(body)
+	response, err := httpClient.Post(fmt.Sprintf("%s/derivation", c.config.PreimageMakerEndpoint), "application/json", buffer)
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+	preimageData, err := io.ReadAll(response.Body)
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+	return preimageData, nil
 }
