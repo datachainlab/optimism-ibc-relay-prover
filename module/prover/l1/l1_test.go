526dc39422c76642eef2e372ea8dbbc79f0e0ef1
yoshidan <naohiro.y@gmail.com>
2025-01-13 23:00:31 +0900

    add l1 test
    Signed-off-by: yoshidan <naohiro.y@gmail.com>

---
+++ module/prover/l1/l1_test.go
@@ -0,0 +1,73 @@
+package l1
+
+import (
+	"context"
+	"fmt"
+	"github.com/datachainlab/optimism-ibc-relay-prover/module/types"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/hyperledger-labs/yui-relayer/log"
+	"github.com/stretchr/testify/suite"
+	"testing"
+	"time"
+)
+
+type L1TestSuite struct {
+	suite.Suite
+	l1Client *L1Client
+}
+
+func TestL1TestSuite(t *testing.T) {
+	suite.Run(t, new(L1TestSuite))
+}
+
+func (ts *L1TestSuite) SetupTest() {
+	err := log.InitLogger("DEBUG", "text", "stdout")
+	ts.Require().NoError(err)
+
+	//TODO Use lighthouse devnet
+	l1ExecutionEndpoint := "http://localhost:8546"
+	l1BeaconEndpoint := "http://localhost:19596"
+
+	l1Client, err := NewL1Client(context.Background(), l1BeaconEndpoint, l1ExecutionEndpoint)
+	ts.Require().NoError(err)
+	ts.l1Client = l1Client
+
+}
+func (ts *L1TestSuite) TestBuildL1Config() {
+	finality, err := ts.l1Client.beaconClient.GetLightClientFinalityUpdate()
+	ts.Require().NoError(err)
+	state, err := ts.l1Client.BuildInitialState(finality.Data.FinalizedHeader.Execution.BlockNumber)
+	ts.Require().NoError(err)
+	l1Config, err := ts.l1Client.BuildL1Config(state)
+	ts.Require().NoError(err)
+
+	bytes, err := l1Config.Marshal()
+	ts.Require().NoError(err)
+	fmt.Println(common.Bytes2Hex(bytes))
+}
+
+func (ts *L1TestSuite) TestGetLatestFinalizedL1Header() {
+	l1Header, err := ts.l1Client.GetLatestFinalizedL1Header()
+	ts.Require().NoError(err)
+
+	// Not additional period
+	finalizedSlot := l1Header.ConsensusUpdate.FinalizedHeader.Slot
+	ctx := context.Background()
+	pastPeriod, err := ts.l1Client.GetSyncCommitteeBySlot(ctx, finalizedSlot, l1Header)
+	ts.Require().NoError(err)
+	ts.Require().Len(pastPeriod, 0)
+
+	// With sync committee
+	ts.Require().True(l1Header.TrustedSyncCommittee != nil)
+	bytes, err := l1Header.Marshal()
+	ts.Require().NoError(err)
+
+	consState := &types.ConsensusState{
+		L1Slot:                 finalizedSlot - 1,
+		L1CurrentSyncCommittee: l1Header.TrustedSyncCommittee.SyncCommittee.AggregatePubkey,
+	}
+	fmt.Println(common.Bytes2Hex(bytes))
+	fmt.Println(consState.L1Slot)
+	fmt.Println(common.Bytes2Hex(consState.L1CurrentSyncCommittee))
+	fmt.Println(time.Now().Unix())
+}
