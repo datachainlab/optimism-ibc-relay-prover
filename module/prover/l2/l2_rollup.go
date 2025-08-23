ba15926dd0294c5e1dada64f445018ce3b32ae53
yoshidan <naohiro.y@gmail.com>
2025-08-23 15:43:53 +0900

    modify rpc

--- module/prover/l2/l2_rollup.go
+++ module/prover/l2/l2_rollup.go
@@ -1,96 +1,39 @@
 package l2
 
 import (
-	"bytes"
 	"context"
 	"encoding/json"
 	"github.com/cockroachdb/errors"
 	"github.com/ethereum/go-ethereum/common/hexutil"
-	"io"
+	"github.com/ethereum/go-ethereum/rpc"
 	"net/http"
 )
 
 func (c *L2Client) SyncStatus(ctx context.Context) (*SyncStatus, error) {
-	data, err := c.call(ctx, "optimism_syncStatus", nil)
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to call syncStatus")
-	}
-	var syncStatus SyncStatus
-	if err = json.Unmarshal(data, &syncStatus); err != nil {
-		return nil, errors.Wrap(err, "failed to unmarshal syncStatus")
-	}
-	c.logger.InfoContext(ctx, "SyncStatus", "finalizedL2", syncStatus.FinalizedL2.Number)
-	return &syncStatus, nil
+	var resp SyncStatus
+	err := c.call(ctx, "optimism_syncStatus", &resp)
+	return &resp, err
 }
 
 func (c *L2Client) RollupConfigBytes(ctx context.Context) ([]byte, error) {
-	data, err := c.call(ctx, "optimism_rollupConfig", nil)
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to get rollup config")
-	}
-	return data, nil
+	var resp json.RawMessage
+	err := c.call(ctx, "optimism_rollupConfig", &resp)
+	return resp, err
 }
 
 func (c *L2Client) OutputAtBlock(ctx context.Context, blockNumber uint64) (*OutputResponse, error) {
-	data, err := c.call(ctx, "optimism_outputAtBlock", []interface{}{hexutil.Uint64(blockNumber)})
-	if err != nil {
-		return nil, errors.Wrapf(err, "failed to get output at block: number=%d", blockNumber)
-	}
-	var outputResponse OutputResponse
-	if err = json.Unmarshal(data, &outputResponse); err != nil {
-		return nil, errors.Wrapf(err, "failed to unmarshal output root: number=%d", blockNumber)
-	}
-	return &outputResponse, nil
+	var resp OutputResponse
+	err := c.call(ctx, "optimism_outputAtBlock", &resp, hexutil.Uint64(blockNumber))
+	return &resp, err
 }
 
-func (c *L2Client) call(ctx context.Context, method string, params []interface{}) ([]byte, error) {
-	type RpcRequest struct {
-		JsonRPC string        `json:"jsonrpc"`
-		Method  string        `json:"method"`
-		Params  []interface{} `json:"params"`
-		Id      int           `json:"id"`
-	}
-	request := &RpcRequest{
-		JsonRPC: "2.0",
-		Method:  method,
-		Params:  params,
-		Id:      1,
-	}
-	body, err := json.Marshal(request)
-	if err != nil {
-		return nil, errors.Wrapf(err, "failed to marshal op-node request: method=%s", method)
-	}
-
+func (c *L2Client) call(ctx context.Context, method string, result interface{}, args ...interface{}) error {
 	httpClient := http.Client{
 		Timeout: c.opNodeTimeout,
 	}
-	httpRequest, err := http.NewRequestWithContext(ctx, "POST", c.opNodeEndpoint, bytes.NewBuffer(body))
-	if err != nil {
-		return nil, errors.Wrapf(err, "failed to make http request for op-node: method=%s", method)
+	jsonClient, err := rpc.DialOptions(ctx, c.opNodeEndpoint, rpc.WithHTTPClient(&httpClient))
+	if err = jsonClient.CallContext(ctx, result, method, args...); err != nil {
+		return errors.Wrapf(err, "failed to make http request for op-node: method=%s", method)
 	}
-	httpRequest.Header.Set("Content-Type", "application/json")
-	response, err := httpClient.Do(httpRequest)
-	if err != nil {
-		return nil, errors.Wrapf(err, "failed to call op-node: method=%s", method)
-	}
-	defer response.Body.Close()
-	if response.StatusCode != http.StatusOK {
-		return nil, errors.Errorf("failed to get sync status: %d", response.StatusCode)
-	}
-	buf, err := io.ReadAll(response.Body)
-	if err != nil {
-		return nil, errors.Wrapf(err, "failed to read response from op-node: method=%s", method)
-	}
-
-	type RpcResult struct {
-		Result json.RawMessage `json:"result"`
-	}
-	var result RpcResult
-	if err = json.Unmarshal(buf, &result); err != nil {
-		return nil, errors.Wrapf(err, "failed to unmarshal from op-node: method=%s", method)
-	}
-	if result.Result == nil {
-		return nil, errors.Errorf("response has no result from op-node: value=%s, method=%s", string(buf), method)
-	}
-	return result.Result, err
+	return nil
 }
